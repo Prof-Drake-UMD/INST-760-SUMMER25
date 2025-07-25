ae3d9ab7af51802474e34fbdb236cbde9b078b54
Vasshanm <vas.shan.d@gmail.com>
2025-07-24 22:59:11 -0400

    Update Prj1.py

--- vs/project_1/Prj1.py
+++ vs/project_1/Prj1.py
@@ -1,9 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pathlib import Path
+import os
+
+# Get the path of the current file
+current_file_path = Path(__file__).resolve()
+# Get the parent directory
+parent_directory = current_file_path.parent
+
+#print(f"Current file path: {current_file_path}")
+#print(f"Parent directory: {parent_directory}")
+
+filename = "Credit_Data.csv"
+data_file_path= os.path.join(parent_directory, filename)
+
 sns.set_theme(style="whitegrid")
 
-df= pd.read_csv('')
+df= pd.read_csv('data_file_path')
 
 
 Ratesort =df.sort_values("Rating",ascending=False)
