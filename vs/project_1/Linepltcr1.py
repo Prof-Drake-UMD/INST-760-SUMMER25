88d1d004f95aa04edf8c6f33efe3b6823d797db9
Vasshanm <vas.shan.d@gmail.com>
2025-07-24 22:55:40 -0400

    Update Linepltcr1.py

--- vs/project_1/Linepltcr1.py
+++ vs/project_1/Linepltcr1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df= pd.read_csv('c:/UMDFILES/UMD/Summer2025/Summer-2-2025/Project/Credit_Data.csv')
+df= pd.read_csv('')
 sns.relplot(x="Education",
             y="Income",
             data=df,
