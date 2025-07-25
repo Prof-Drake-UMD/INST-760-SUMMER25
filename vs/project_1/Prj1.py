ec10771a256462b35f68f88a8164a54aadbec134
Vasshanm <vas.shan.d@gmail.com>
2025-07-24 21:54:33 -0400

    Add files via upload

---
+++ vs/project_1/Prj1.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme(style="whitegrid")
+
+df= pd.read_csv('c:/UMDFILES/UMD/Summer2025/Summer-2-2025/Project/Credit_Data.csv')
+
+
+Ratesort =df.sort_values("Rating",ascending=False)
+
+
+sns.barplot(x="Rating",
+            y="Ethnicity",
+            data=Ratesort,
+            errorbar=('ci',0),
+            hue="Student"
+             )
+        
+plt.xticks(rotation=90)
+plt.show()
+
+
+g=sns.catplot(x="Ethnicity",
+                y="Rating",
+                data=df, kind="box",
+                whis=[0,100],
+              col="Gender",
+              hue="Ethnicity"
+              )
+
+
+plt.xticks(rotation=90)
+plt.show()
+
+
+sns.catplot(x="Ethnicity",
+            data=df,
+            hue="Ethnicity",
+            kind="count")
+
+plt.xticks(rotation=90)
+plt.show()
+
+sns.relplot(x="Education",
+            y="Income",
+            data=df,
+            kind="line",
+            errorbar=None)
+plt.xticks(rotation=90)
+plt.show()
+
+
+sns.relplot(x="Cards",
+                y="Rating",
+                data=df, kind="scatter", hue="Married")
+
+plt.xticks(rotation=90)
+plt.show()
+             
+
+
