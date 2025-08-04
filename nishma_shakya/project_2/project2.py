21902d59a7363692c687047f395ae6d3decbf59a
Nishma <ns4606@nyu.edu>
2025-08-04 19:38:38 -0400

    added caption

--- nishma_shakya/project_2/project2.py
+++ nishma_shakya/project_2/project2.py
@@ -24,10 +24,15 @@ g = sns.relplot(data=df, x="percent_low_income", y="avg_test_score_percent", kin
 # labels/titles
 g.set_titles("Grade Level: {col_name}")
 g.set_axis_labels("Percent Low Income (%)", "Average Test Score (%)")
-g.fig.suptitle("The Impact of Student Household Income on Student Test Scores", fontsize=16)
+g.fig.suptitle("The Impact of Student Household Income on Student Test Scores in New York", fontsize=16)
+# brief caption to explain 
+g.fig.text(0.5, 0.01, 
+           "Figure: Relationship between the percentage of low-income students and their test scores in New York schools. "
+           "Point size represents the amount of funding per student, and color indicates school type.", 
+           ha='center', fontsize=10,fontstyle='italic')
 # adjusting (bc y-axis starts at 40... want it to start at 0%)
 g.set(ylim=(0, None))
-g.fig.subplots_adjust(top=0.85)
+g.fig.subplots_adjust(top=0.85, bottom=0.15)
 plt.savefig("projct2.png")
 plt.show()
 plt.clf()
